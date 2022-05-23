58fe26941f870416730f9c322fcbac39b2dcb93e
IREM.UMUT <irem.umut@lcwaikiki.com>
2022-05-23 16:30:29 +0300

    changed the laguage

--- client/src/components/SignInForm.js
+++ client/src/components/SignInForm.js
@@ -6,30 +6,30 @@ import PrimaryButton from "./PrimaryButton";
 const SignInForm = () => {
   return (
     <div className="px-8 py-9 flex flex-col mx-2 sm:mx-16 my-20 lg:mx-0 font-sans border border-[#878787] border-[0.5px] rounded-lg shadow-xl">
-      <p className="font-light text-2xl py-9">Hoş Geldiniz!</p>
-      <p className="signin pb-8 font-medium text-3xl">Giriş Yap</p>
+      <p className="font-light text-2xl py-9">Welcome!</p>
+      <p className="signin pb-8 font-medium text-3xl">Sign In</p>
       <form>
         <FormInputLabel
-          inputId={"name"}
-          inputType={"text"}
-          label=" Kullanıcı adı"
-          placeholder="Kullanıcı adınızı girin"
+          inputId={"email"}
+          inputType={"email"}
+          label=" Email"
+          placeholder="Please enter your email"
         />
         <FormInputLabel
           inputId={"password"}
           inputType={"password"}
-          label="Şifre"
-          placeholder="Şifrenizi girin"
+          label="Password"
+          placeholder="Please enter your password"
         />
 
         <div className="flex flex-row justify-between pb-9">
           <div className="flex align-center">
             <input type="checkbox" className="w-4 h-4"/>
-            <p className="inline px-2 text-xs text-slate-500">Beni hatırla</p>
+            <p className="inline px-2 text-xs text-slate-500">Remember me</p>
           </div>
           <Link to="/">
           <p className="text-xs text-slate-500">
-            Şifremi unuttum
+            Forgot password
           </p>
           </Link>
         </div>
@@ -37,8 +37,8 @@ const SignInForm = () => {
         <PrimaryButton text="Giriş Yap" witdh="w-full"/>
         
         <p className="text-center mt-9 text-slate-500 text-base" >
-          Hesabın yok mu? 
-          <Link to={"/signup"} className="text-black font-bold"> Kayıt Ol</Link>
+          Do not have an account? 
+          <Link to={"/signup"} className="text-black font-bold"> Sign Up</Link>
         </p>
       </form>
     </div>
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -7,45 +7,45 @@ import PrimaryButton from './PrimaryButton';
 function SignUpForm() {
     return (
     <div className="px-8 py-9 flex flex-col mx-2 sm:mx-16 my-20 lg:mx-0 font-sans border border-[#878787] border-[0.5px] rounded-lg shadow-xl">
-      <p className="font-light text-2xl py-9">Hoş Geldiniz!</p>
-      <p className="signin pb-8 font-medium text-3xl">Kayıt Ol</p>
+      <p className="font-light text-2xl py-9">Welcome!</p>
+      <p className="signin pb-8 font-medium text-3xl">Sign Up</p>
       <form>
           <FormInputLabel
           inputId={"email"}
           inputType={"email"}
           label="Email"
-          placeholder="Emalinizi girin"
+          placeholder="Please enter your email"
         />
         <FormInputLabel
           inputId={"name"}
           inputType={"text"}
-          label="Ad soyad"
-          placeholder="Ad ve soyadınızı girin"
+          label="Name"
+          placeholder="Please enter your name"
         />
         <FormInputLabel
           inputId={"username"}
           inputType={"text"}
-          label="Kullanıcı adı"
-          placeholder="@ Kullanıcı adınızı girin"
+          label="Username"
+          placeholder="@ Please enter your username"
         />
         <FormInputLabel
           inputId={"password"}
           inputType={"password"}
-          label="Şifre"
-          placeholder="Şifrenizi girin"
+          label="Password"
+          placeholder="Please enter your password"
         />
         <FormInputLabel
           inputId={"passwordConfirm"}
           inputType={"password"}
-          label="Şifreyi Onayla"
-          placeholder="Şifrenizi tekrar girin"
+          label="Confirm Password"
+          placeholder="Please enter your password again"
         />
 
-        <PrimaryButton text="Kayıt Ol" witdh="w-full"/>
+        <PrimaryButton text="Sign Up" witdh="w-full"/>
         
         <p className="text-center mt-9 text-slate-500 text-base" >
-          Zaten hesabınız var mı?
-          <Link to={"/signin"} className="text-black font-bold"> Giriş Yap</Link>
+          Already have an account?
+          <Link to={"/signin"} className="text-black font-bold"> Sign In</Link>
         </p>
       </form>
     </div>
