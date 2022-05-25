0aba7ad6975f1ee59ffb46058267394e2538e726
iremumut <irem2000.nur@gmail.com>
2022-05-25 11:15:27 +0300

    installed redux toolkit and set up redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Link, Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
