4c231d7d703bef431fed01c754d758b1a02d24d5
iremumut <irem2000.nur@gmail.com>
2022-05-28 14:45:11 +0300

    styled the dropdown menu

--- client/src/pages/components/NavBar.js
+++ client/src/pages/components/NavBar.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 import searchIcon from "../../assets/vectors/search.svg";
+import dropdownArrow from "../../assets/vectors/dropdown.svg";
+import { useState } from "react";
 const NavBar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const changeDropdown = () => {
+    setDropdown((prev) => !prev);
+  };
   return (
-    <div className="py-4 sm:px-16 px-2 flex flex-row items-center ">
+    <div className="py-4 sm:px-16 px-2 flex flex-row items-start ">
       <Link to={"/"}>
         <p className="sm:text-4xl text-2xl font-semibold xl:mr-28 mr-4">OLTA</p>
       </Link>
@@ -21,22 +28,37 @@ const NavBar = () => {
         </Link>
         <button className="hover:text-[#4A5567]/70 px-1 ">Sign Out</button>
       </div>
-      <div className="md:hidden mx-4">
-        <select>
-          <option>
-            {" "}
-            <button className="hover:text-[#4A5567]/70 ">New Post</button>
-          </option>
-          <option>
-            {" "}
-            <Link to={"/"}>
-              <p className="hover:text-[#4A5567]/70  ">Wallet</p>
-            </Link>
-          </option>
-          <option>
-            <button className="hover:text-[#4A5567]/70">Sign Out</button>
-          </option>
-        </select>
+      <div className="md:hidden mx-4 text-[#4A5567] bg-neutral-50/[.97] rounded-lg py-2 w-32 shadow ">
+        <button className="px-4" onClick={changeDropdown}>
+          <p className="inline">Menu</p>
+          <img src={dropdownArrow} className="inline ml-6" alt="" />
+        </button>
+        <div className={`self-start ${dropdown ? "inline" : "hidden"}`}>
+          <Link
+            className="hover:text-black hover:bg-neutral-300 px-4 py-2 block"
+            to={"/"}
+          >
+            New Post
+          </Link>
+          <Link
+            className="hover:text-black hover:bg-neutral-300 px-4 py-2 block"
+            to={"/"}
+          >
+            Wallet
+          </Link>
+          <Link
+            className="hover:text-black hover:bg-neutral-300 px-4 py-2 block"
+            to={"/"}
+          >
+            Profile
+          </Link>
+          <Link
+            className="hover:text-black hover:bg-neutral-300 px-4 py-2 block"
+            to={"/"}
+          >
+            Sign Out
+          </Link>
+        </div>
       </div>
       <div className="xl:basis-5/12 md:basis-3/12 flex flex-row ml-auto justify-end items-center items-end xsm:flex hidden ">
         <p className="mx-2 text-[#4E5D78] lg:text-xl text-base lg:font-semibold	font-normal hidden lg:inline hover:text-[#4A5567]/70">
@@ -44,7 +66,7 @@ const NavBar = () => {
         </p>
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8FuEJbKwDdaz1h387130xmYkAIQbZpahhbQ&usqp=CAU"
-          className="lg:h-12 lg:w-12 w-8 h-8 lg:rounded-xl ml-2 rounded-full "
+          className="lg:h-12 lg:w-12 w-8 h-8 lg:rounded-xl ml-2 rounded-full object-cover"
           alt="profile"
         ></img>
       </div>
@@ -54,6 +76,7 @@ const NavBar = () => {
 
 export default NavBar;
 
+/*className={dropdown ? "inline" : "hidden"}*/
 // "text-[#4A5567]"
 
 /* 
@@ -103,4 +126,22 @@ export default NavBar;
         ></img>
       </div>
     </div>
+
+
+
+    <select>
+          <option>
+            {" "}
+            <button className="hover:text-[#4A5567]/70 ">New Post</button>
+          </option>
+          <option>
+            {" "}
+            <Link to={"/"}>
+              <p className="hover:text-[#4A5567]/70  ">Wallet</p>
+            </Link>
+          </option>
+          <option>
+            <button className="hover:text-[#4A5567]/70">Sign Out</button>
+          </option>
+        </select>
 */
