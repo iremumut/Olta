f6fba2e1a72e172eef44883e529e25663a29c1b8
iremumut <irem2000.nur@gmail.com>
2022-06-11 01:48:28 +0300

    finished transaction page

--- client/src/pages/Transaction.js
+++ client/src/pages/Transaction.js
@@ -12,6 +12,7 @@ import {
   reset,
   sendTransaction,
 } from "../features/transactions/transactionSlice";
+import ethereum from "../assets/vectors/ethereum.svg";
 
 const Transaction = () => {
   const location = useLocation();
@@ -79,14 +80,35 @@ const Transaction = () => {
     return <p>Transaction is happening....</p>;
   }
   return (
-    <div className="flex flex-row justify-center">
-      <div className="bg-[#EAEEF3] p-4 px-12 rounded-xl flex flex-col justify-start">
-        <p className="bg-white text-[#4E8BFF] font-semibold text-2xl px-4 py-2 rounded-xl self-start">
-          Buy Content
-        </p>
-        <SimplePost post={post} creator={creator} />
-        <div className="bg-white my-4 "></div>
-        <button onClick={handleBuy}>Buy</button>
+    <div className="flex justify-center">
+      <div className="flex flex-row justify-center w-2/3">
+        <div className="bg-[#EAEEF3] p-4 px-24 rounded-xl flex flex-col justify-start">
+          <p className="bg-white text-[#4E8BFF] font-semibold text-2xl px-4 py-2 rounded-xl self-start">
+            Buy Content
+          </p>
+          <SimplePost post={post} creator={creator} />
+          <div className="bg-white my-4 flex flex-col justify-center px-10 py-6 rounded-xl">
+            <p className="text-base font-medium text-center">
+              Do you want to support{" "}
+              <span className="text-[#0368FF]">{creator.name}</span>'s content?
+            </p>
+            <div className="bg-[#EBF2FF] flex flex-col items-center p-4 px-8 my-4 self-center rounded-xl">
+              <p className="text-[#4A5567] font-bold text-2xl">PRICE</p>
+              <div className="flex flex-row justify-center items-center py-4">
+                <p className="text-[#0368FF] font-medium text-2xl">
+                  {post.price}
+                </p>
+                <img src={ethereum} alt="" className="" />
+              </div>
+              <button
+                className="bg-white border border-[#0368FF] text-[#0368FF] py-2 px-8 rounded-xl hover:bg-[#0368FF] hover:text-white"
+                onClick={handleBuy}
+              >
+                PAY
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
